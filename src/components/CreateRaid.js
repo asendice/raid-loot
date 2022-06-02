824b1cd98827756f90a37e2c905afb29dc56b4c9
Dylan Travis <dylan.travis23@outlook.com>
2022-06-02 15:11:02 -0700

    raid state and button onClick

--- src/components/CreateRaid.js
+++ src/components/CreateRaid.js
@@ -1,8 +1,13 @@
-function CreateRaid() {
+import { useState } from "react";
+
+function CreateRaid({ updateRaids }) {
+  const [raid, setRaid] = useState("");
+
   return (
     <div>
       <h1>Create Raid</h1>
-      <input type="text" />
+      <input type="text" onChange={(e) => setRaid(e.target.value)} />
+      <button onClick={() => updateRaids(raid)}>submit</button>
     </div>
   );
 }
